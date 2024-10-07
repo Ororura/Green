4b9f0dc4696bcc3d90995a7dc3de27decd308e32
Ororura <hitmanlol890@gmail.com>
2024-10-07 18:36:09 +0300

    Initial Commit

---
+++ Green/GreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenApp.swift
+//  Green
+//
+//  Created by Egor Gladkikh on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenTests/GreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenTests.swift
+//  GreenTests
+//
+//  Created by Egor Gladkikh on 07.10.2024.
+//
+
+import Testing
+@testable import Green
+
+struct GreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
